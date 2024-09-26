151b77ac3534472791592143d7ebdbfab63acba6
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2024-09-26 14:34:07 +0400

    Create Calculate.java

---
+++ laba2/Task3/Calculate.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Calculate {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль невозможно.");
+        }
+        return a / b;
+    }
+}
