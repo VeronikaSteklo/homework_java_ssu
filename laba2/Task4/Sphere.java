611de7bd2282ccf7eda572f6ecb658dcd1cbe6f4
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2024-10-07 19:31:00 +0400

    Create Sphere.java

---
+++ laba2/Task4/Sphere.java
@@ -0,0 +1,14 @@
+package org.example.Task4;
+
+class Sphere extends Shape {
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+}
